f8b1e696a3482bbcc8d6252ffd8caf74497d6a5a
Sewavi Grace <148196064+Grace-Sewavi@users.noreply.github.com>
2025-07-14 03:06:19 -0700

    Create RobotChild.sol

---
+++ 16_Inheritance/RobotChild.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./RobotParent.sol"; // Bring in the parent!
+
+contract RobotChild is RobotParent {
+    // Inherits greet() from RobotParent
+}
